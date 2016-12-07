538e711f1662304c27ef8a99829ad2f7e5659679
tmazza <tiagomdepaula@gmail.com>
2016-12-06 23:25:01 -0200

    11286 - Conformity

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Disciplina de Desafios de Programação
 
 [10954 - Add All](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=21&page=show_problem&problem=1895) | [Solution](code/10954.cpp) 
 
+[11286 - Conformity](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=16&page=show_problem&problem=2261) | [Solution](code/11286.cpp)
+
 [11459 - Snakes and Ladders](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=26&page=show_problem&problem=2454) | [Solution](code/11459.cpp)
 
 [11559 - Event Planning](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=27&page=show_problem&problem=2595) | [Solution](code/11559.cpp)
---
+++ code/11286.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <functional>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int  main() {
+  // const clock_t begin_time = clock();
+  int c;
+  std::vector<int> f(5,0);
+  std::map<string,int> ids;
+  while(cin >> c){
+    if(c == 0){ break; }
+
+    for(int i=0;i<c;i++){
+      cin >> f[0] >> f[1] >> f[2] >> f[3] >> f[4];
+      sort(f.begin(),f.end());
+      std::stringstream ss;
+      ss << f[0] << "." << f[1] << "." << f[2] << "." << f[3] << "." << f[4];
+      std::string s = ss.str();
+      if(ids.find(s) == ids.end()){
+        ids.insert ( std::pair<string,int>(s,1) );
+      } else {
+        ids[s] = ids[s] + 1;
+      }
+    }
+
+    std::map<string,int>::iterator it;
+    int maior = 0;
+    for (it=ids.begin(); it!=ids.end(); ++it){
+      if(it->second>maior){
+        maior = it->second;
+      }
+    }
+    int sum = 0;
+    for (it=ids.begin(); it!=ids.end(); ++it){
+      if(it->second==maior){
+        sum+=maior;
+      }
+    }
+    cout << sum << "\n";
+    ids.clear();
+  }
+  // cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << "\n";
+}
